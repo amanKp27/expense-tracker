5e3428bfa5115e8ca6cac649dcb75930455a864f
Aman Kumar Poddar <amankrpoddar18@gmail.com>
2026-01-15 17:24:16 +0530

    Fixing case sensitivity for deployment

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -7,7 +7,7 @@ import {Link} from 'react-router-dom';
 import { validateEmail } from '../../utils/helper';
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from "../../utils/apiPaths";
-import { UserContext } from '../../context/userContext';
+import { UserContext } from '../../context/UserContext';
 
 function Login() {
   const [email,setEmail] = useState("");
